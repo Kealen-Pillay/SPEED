7ad1e874e7c98071228b91e8cee90cc83814f468
Kealen-Pillay <kealenpillay@gmail.com>
2022-09-29 12:03:03 +1300

    removed unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Homepage } from "./components/homepage/Homepage";
 import { SubmitForm } from "./components/submitForm/SubmitForm";
 
 function App() {

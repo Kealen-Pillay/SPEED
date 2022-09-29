ae5fc7532c484ab11e872d7d27e2b28b2a081f81
Kealen-Pillay <kealenpillay@gmail.com>
2022-09-29 18:48:26 +1300

    Handled form fields with state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import { SubmitForm } from "./components/submitForm/SubmitForm";
+import { Homepage } from "./components/homepage/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <SubmitForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SubmitForm />} />
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/submitForm/SubmitForm.js
+++ frontend/src/components/submitForm/SubmitForm.js
@@ -12,10 +12,16 @@ export const SubmitForm = () => {
   const [researchType, setResearchType] = useState("");
   const [author, setAuthor] = useState("");
   const [description, setDescription] = useState("");
+  const [publishedDate, setPublishedDate] = useState();
+  const [publisher, setPublisher] = useState("");
+
+  const navigateHome = () => {
+    console.log("home");
+  };
 
   return (
     <body>
-      <button id="homeButton">
+      <button id="homeButton" onClick={navigateHome}>
         <h6 className="gradient-text">Home</h6>
       </button>
       <button id="searchButton">
@@ -24,53 +30,90 @@ export const SubmitForm = () => {
       <div className="container">
         <form>
           <h1>Submit Article</h1>
-          <input type="text" required placeholder="Title" value={title}></input>
+          <input
+            type="text"
+            required
+            placeholder="Title"
+            value={title}
+            onChange={(title) => setTitle(title.target.value)}
+          ></input>
           <input
             type="text"
             required
             placeholder="Journal Name"
             value={journalName}
+            onChange={(journal) => setJournalName(journal.target.value)}
           ></input>
           <input
             type="number"
             required
             placeholder="Volume"
             value={volume}
+            onChange={(volume) => setVolume(volume.target.value)}
           ></input>
-          <input type="text" required placeholder="Pages" value={pages}></input>
-          <input type="text" required placeholder="DOI" value={DOI}></input>
+          <input
+            type="text"
+            required
+            placeholder="Pages"
+            value={pages}
+            onChange={(pages) => setPages(pages.target.value)}
+          ></input>
+          <input
+            type="text"
+            required
+            placeholder="DOI"
+            value={DOI}
+            onChange={(DOI) => setDOI(DOI.target.value)}
+          ></input>
           <input
             type="text"
             required
             placeholder="Practice"
             value={practice}
+            onChange={(practice) => setPractice(practice.target.value)}
           ></input>
-          <input type="text" required placeholder="Claim" value={claim}></input>
+          <input
+            type="text"
+            required
+            placeholder="Claim"
+            value={claim}
+            onChange={(claim) => setClaim(claim.target.value)}
+          ></input>
           <input
             type="text"
             required
             placeholder="Research Type"
             value={researchType}
+            onChange={(researchType) => setResearchType(researchType.target.value)}
           ></input>
           <input
             type="text"
             required
             placeholder="Author"
             value={author}
+            onChange={(author) => setAuthor(author.target.value)}
           ></input>
           <input
             type="text"
             required
             placeholder="Description"
             value={description}
+            onChange={(description) => setDescription(description.target.value)}
           ></input>
           <input
             id="publishedDate"
             type="date"
             placeholder="Published Date"
             required
+            onChange={(date) => setPublishedDate(date.target.value)}
           ></input>
-          <input type="text" required placeholder="Publisher"></input>
+          <input
+            type="text"
+            required
+            placeholder="Publisher"
+            value={publisher}
+            onChange={(publisher) => setPublisher(publisher.target.value)}
+          ></input>
           <button>
             <h6 className="gradient-text">Submit Article</h6>
           </button>

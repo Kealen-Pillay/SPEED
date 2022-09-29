959faba1637c4bc256072bdd5a1e4929d7ca7106
Kealen-Pillay <kealenpillay@gmail.com>
2022-09-29 21:43:59 +1300

    Styleed homepage

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./Homepage.css";
 
 export const Homepage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="titleContainer">
+      <h1 className="title-text">SPEED</h1>
+    </div>
+  );
 };

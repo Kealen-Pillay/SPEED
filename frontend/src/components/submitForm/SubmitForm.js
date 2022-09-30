71752bb882248fb04b386fdc01457584aa032b0e
kyleCS50 <kyledanielfrancis@gmail.com>
2022-10-01 11:47:21 +1300

    Choose between form and bib tex

--- frontend/src/components/submitForm/SubmitForm.js
+++ frontend/src/components/submitForm/SubmitForm.js
@@ -16,6 +16,7 @@ export const SubmitForm = () => {
   const [description, setDescription] = useState("");
   const [publishedDate, setPublishedDate] = useState();
   const [publisher, setPublisher] = useState("");
+  const [formatType, setFormatType] = useState("");
 
   const navigate = useNavigate();
 
@@ -70,98 +71,111 @@ export const SubmitForm = () => {
         <h6 className="gradient-text">Search Articles</h6>
       </button>
       <div className="container">
-        <form id="form" onSubmit={postArticle}>
-          <h1>Submit Article</h1>
-          <input
-            type="text"
-            required
-            placeholder="Title"
-            value={title}
-            onChange={(title) => setTitle(title.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Journal Name"
-            value={journalName}
-            onChange={(journal) => setJournalName(journal.target.value)}
-          ></input>
-          <input
-            type="number"
-            required
-            placeholder="Volume"
-            value={volume}
-            onChange={(volume) => setVolume(volume.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Pages"
-            value={pages}
-            onChange={(pages) => setPages(pages.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="DOI"
-            value={DOI}
-            onChange={(DOI) => setDOI(DOI.target.value)}
-          ></input>
-          <select onChange={(practice) => setPractice(practice.target.value)}>
-            <option value="" disabled selected>
-              Practice
-            </option>
-            <option value="TDD">TDD</option>
-            <option value="BDD">BDD</option>
-          </select>
-          <input
-            type="text"
-            required
-            placeholder="Claim"
-            value={claim}
-            onChange={(claim) => setClaim(claim.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Research Type"
-            value={researchType}
-            onChange={(researchType) =>
-              setResearchType(researchType.target.value)
-            }
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Author"
-            value={author}
-            onChange={(author) => setAuthor(author.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Description"
-            value={description}
-            onChange={(description) => setDescription(description.target.value)}
-          ></input>
-          <input
-            id="publishedDate"
-            type="date"
-            placeholder="Published Date"
-            required
-            onChange={(date) => setPublishedDate(date.target.value)}
-          ></input>
-          <input
-            type="text"
-            required
-            placeholder="Publisher"
-            value={publisher}
-            onChange={(publisher) => setPublisher(publisher.target.value)}
-          ></input>
-          <button type="submit">
-            <h6 className="gradient-text">Submit Article</h6>
-          </button>
-        </form>
+        <h1>Submit Article</h1>
+        <select onChange={(format) => setFormatType(format.target.value)}>
+          <option value="" disabled selected>
+            Select a Upload Format
+          </option>
+          <option value="form">Form</option>
+          <option value="bibtex">BibTeX</option>
+        </select>
+        {formatType === "form" ? (
+          <form id="form" onSubmit={postArticle}>
+            <input
+              type="text"
+              required
+              placeholder="Title"
+              value={title}
+              onChange={(title) => setTitle(title.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Journal Name"
+              value={journalName}
+              onChange={(journal) => setJournalName(journal.target.value)}
+            ></input>
+            <input
+              type="number"
+              required
+              placeholder="Volume"
+              value={volume}
+              onChange={(volume) => setVolume(volume.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Pages"
+              value={pages}
+              onChange={(pages) => setPages(pages.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="DOI"
+              value={DOI}
+              onChange={(DOI) => setDOI(DOI.target.value)}
+            ></input>
+            <select onChange={(practice) => setPractice(practice.target.value)}>
+              <option value="" disabled selected>
+                Practice
+              </option>
+              <option value="TDD">TDD</option>
+              <option value="BDD">BDD</option>
+            </select>
+            <input
+              type="text"
+              required
+              placeholder="Claim"
+              value={claim}
+              onChange={(claim) => setClaim(claim.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Research Type"
+              value={researchType}
+              onChange={(researchType) =>
+                setResearchType(researchType.target.value)
+              }
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Author"
+              value={author}
+              onChange={(author) => setAuthor(author.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Description"
+              value={description}
+              onChange={(description) =>
+                setDescription(description.target.value)
+              }
+            ></input>
+            <input
+              id="publishedDate"
+              type="date"
+              placeholder="Published Date"
+              required
+              onChange={(date) => setPublishedDate(date.target.value)}
+            ></input>
+            <input
+              type="text"
+              required
+              placeholder="Publisher"
+              value={publisher}
+              onChange={(publisher) => setPublisher(publisher.target.value)}
+            ></input>
+            <button type="submit">
+              <h6 className="gradient-text">Submit Article</h6>
+            </button>
+          </form>
+        ) : (
+          <input type="file" />
+        )}
       </div>
     </body>
   );

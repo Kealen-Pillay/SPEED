9dffd4d43e6486e0157784a6d79067ac10f1714d
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-02 20:22:21 +1300

    Trying to deploy

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
+// const express = require("express");
+// const connectDB = require("./config/db");
+// var cors = require("cors");
+
+// // routes
+// const articles = require("./routes/api/articles");
+
+// const app = express();
+
+// // Connect Database
+// connectDB();
+
+// // cors
+// app.use(cors({ origin: true, credentials: true }));
+
+// // Init Middleware
+// app.use(express.json({ extended: false }));
+
+// app.get("/", (req, res) => res.send("Hello world!"));
+
+// // use Routes
+// app.use("/api/articles", articles);
+
+// const port = process.env.PORT || 8082;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 const express = require("express");
-const connectDB = require("./config/db");
 var cors = require("cors");
+const connectDB = require("./config/db");
 
 // routes
-const articles = require("./routes/api/articles");
+const books = require("./routes/api/books");
 
 const app = express();
-
-// Connect Database
-connectDB();
+const port = process.env.PORT || 8082;
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
-// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+app.use("/api/books", books);
 
-// use Routes
-app.use("/api/articles", articles);
-
-const port = process.env.PORT || 8082;
+// Connect Database
+connectDB();
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/frontend/build"));
+    app.get("/", (req,res) => {
+        res.sendFile(__dirname+"/frontend/build/index.html")
+    })
+} else {
+    app.get("*", (req,res) => {
+        res.send(`API is running on port ${port}`)
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

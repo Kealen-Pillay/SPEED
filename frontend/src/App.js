547216214048771e51bd8b315ccf989a335c74cf
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-04 10:49:51 +1300

    Created Data Display Table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
 import { SubmitForm } from "./components/submitForm/SubmitForm";
-import { SearchArticle } from "./components/searchArticle/SearchArticle";
+import { SearchArticle2 } from "./components/searchArticle/SearchArticle2";
 import { Homepage } from "./components/homepage/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    await axios
+      .get("http://localhost:8082/api/articles")
+      .then((res) => {
+        setArticles(res.data);
+      })
+      .catch((err) => {
+        console.log("error:" + err);
+      });
+  };
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/search" element={<SearchArticle />} />
+          <Route
+            path="/search"
+            element={articles && <SearchArticle2 articles={articles} />}
+          />
           <Route path="/post" element={<SubmitForm />} />
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/components/searchArticle/SearchArticle2.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import "./SearchArticle.css";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+
+export const SearchArticle2 = (props) => {
+  const { articles } = props;
+  const [articleCollection, setArticleCollection] = useState([]);
+
+  useEffect(() => {
+    console.log(articles);
+  }, []);
+
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+  const navigateSubmit = () => {
+    navigate("/post");
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", hide: true },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "journal",
+      headerName: "Journal",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "volume",
+      headerName: "Volume",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "pages",
+      headerName: "Pages",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "doi",
+      headerName: "DOI",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "practice",
+      headerName: "Practice",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "claim",
+      headerName: "Claim",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "researchType",
+      headerName: "Research",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "author",
+      headerName: "Author",
+      type: "string",
+      width: 110,
+      editable: false,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      type: "string",
+      width: 110,
+      editable: false,
+    },
+    {
+      field: "publishedDate",
+      headerName: "Published Date",
+      type: "string",
+      width: 110,
+      editable: false,
+    },
+    {
+      field: "publishers",
+      headerName: "Publishers",
+      type: "string",
+      width: 110,
+      editable: false,
+    },
+  ];
+
+  const rows = articles.map((row) => ({
+    id: row._id,
+    title: row.title,
+    journal: row.journal,
+  }));
+
+  return (
+    <body>
+      <button id="homeButton" onClick={navigateHome}>
+        <h6 className="gradient-text">Home</h6>
+      </button>
+      <button id="submitButton" onClick={navigateSubmit}>
+        <h6 className="gradient-text">Submit Article</h6>
+      </button>
+      <div id="contentContainer">
+        <div id="selectionContainer">
+          <select>
+            <option disabled selected>
+              SE Practice
+            </option>
+            <option>TDD</option>
+            <option>BDD</option>
+          </select>
+          <select>
+            <option disabled selected>
+              Claim
+            </option>
+            <option>a</option>
+            <option>b</option>
+          </select>
+          <select>
+            <option disabled selected>
+              Start Publication Year
+            </option>
+            <option>2000</option>
+            <option>2001</option>
+          </select>
+          <select>
+            <option disabled selected>
+              End Publication Year
+            </option>
+            <option>2000</option>
+            <option>2001</option>
+          </select>
+        </div>
+        <Box sx={{ height: 400, width: "80%", marginTop: "5%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+          />
+        </Box>
+        <button id="searchButton">
+          <h6 className="gradient-text">Search</h6>
+        </button>
+      </div>
+    </body>
+  );
+};

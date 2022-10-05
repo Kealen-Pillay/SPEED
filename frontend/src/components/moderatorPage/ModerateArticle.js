41c5796842c2b24cd452686eede5986f52b135d8
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-05 13:24:18 +1300

    Added navigation in the moderator screen to the search and get article  screen

--- frontend/src/components/moderatorPage/ModerateArticle.js
+++ frontend/src/components/moderatorPage/ModerateArticle.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ModerateArticle = () => {
-  return <div>ModerateArticle</div>;
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+  const navigateSubmit = () => {
+    navigate("/post");
+  };
+
+  const navigateSearch = () => {
+    navigate("/search");
+  };
+
+  return (
+    <body>
+      <button id="homeButton" onClick={navigateHome}>
+        <h6 className="gradient-text">Home</h6>
+      </button>
+      <button id="submitButton" onClick={navigateSubmit}>
+        <h6 className="gradient-text">Submit Article</h6>
+      </button>
+      <button id="searchButton" onClick={navigateSearch}>
+        <h6 className="gradient-text">Search Article</h6>
+      </button>
+    </body>
+  );
 };
